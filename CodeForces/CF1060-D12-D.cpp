b3c865c946ca22bc3468e21fc0b0c4f2df360e2f
ahmedsamir221 <45498762+ahmedsamir221@users.noreply.github.com>
2019-06-08 18:26:04 +0200

    Create CF1060-D12-D.cpp

---
+++ CodeForces/CF1060-D12-D.cpp
@@ -0,0 +1,50 @@
+// we will try to match max left with max right to deduce the number of chairs
+#include<queue>
+#include<map>
+#include <string>
+#include<bits/stdc++.h>
+#define pi 3.141592654
+#define forr(i,a,b) for(int i=a;i<=b;i++)
+#define F first
+#define S second
+#define input ios_base::sync_with_stdio(0);cin.tie(0);
+//#define x real()
+//#define y imag()
+using namespace std;
+typedef pair<double,double>pdd;
+typedef long long  ll;
+typedef pair<ll, ll>pii;
+//typedef complex<double> point;
+//template<typename T>T gcd(T x, T y) { if(y == 0)return x; else return gcd(y, x%y); }
+//typedef bitset<30> mask;
+//int x[8]={1,0,0,-1,-1,-1,1,1};
+//int y[8]={0,1,-1,0,-1,1,-1,1};
+//#define var(x) ((x)<<1)
+//#define nvar(x) ((x)^1)
+const int N=210,M=20500;
+
+ll a[200020],b[200020],n,ans;
+ main()
+{
+
+//cout<<Pow((ll)1000000,(ll)110);
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+//    char input[15];
+//    scanf("%s", &input);  array of char
+//  gets(c+1);  array of char
+//printf("%s, ",name[k[i]].c_str());  printf of string
+input;
+
+cin>>n;
+forr(i,1,n)
+    cin>>a[i]>>b[i];
+sort(a+1,a+1+n);
+sort(b+1,b+1+n);
+
+forr(i,1,n)
+ans+=max(a[i],b[i]);
+
+cout<<ans+n;
+return 0;
+}
