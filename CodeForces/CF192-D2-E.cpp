1dbe1ff606c904b63e3ff53589c4c1c8fcb990ef
ahmedsamir221 <45498762+ahmedsamir221@users.noreply.github.com>
2019-07-09 07:10:34 +0200

    Create CF192-D2-E.cpp

---
+++ CodeForces/CF192-D2-E.cpp
@@ -0,0 +1,134 @@
+/*
+    in begin each node has a value = 0
+    in each visit we will increase the value of the two cities by 1 and decrease the value of LCA between them by 2 .
+    then we will put in each node the sum of it's subtree .
+    and the answer of each edge will be the value of the node with high depth from the  root .
+ */
+#include<queue>
+#include <cmath>
+#include <complex>
+#include<map>
+#include <string>
+#include<iostream>
+#include<bits/stdc++.h>
+
+
+#define pi 3.141592654
+#define forr(i,a,b) for(int i=a;i<=b;i++)
+#define F first
+#define S second
+#define input ios_base::sync_with_stdio(0);cin.tie(0);
+
+/*
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep ,cos
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel , sin
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+*/
+
+using namespace std;
+typedef pair<double,double>pdd;
+typedef long long  ll;
+typedef pair<ll, ll>pii;
+typedef complex<double> point;
+//template<typename T>T gcd(T x, T y) { if(y == 0)return x; else return gcd(y, x%y); }
+//typedef bitset<30> mask;
+//int x[8]={1,0,0,-1,-1,-1,1,1};
+//int y[8]={0,1,-1,0,-1,1,-1,1};
+//#define var(x) ((x)<<1)
+//#define nvar(x) ((x)^1)
+const int N=100005,M=100000;
+const double EPS= 1e-8;
+int n,l[N],p[N][20],ck[N],ans[N];
+vector<int>a[N];
+map<pair<int,int>,int>m;
+void dfs(int u,int P)
+{
+    for(auto v:a[u])
+    {
+        if(v==P)continue;
+        else {
+            l[v]=l[u]+1;
+            p[v][0]=u;
+            dfs(v,u);
+        }
+    }
+}
+void pre()
+{
+    forr(j,1,19)
+      forr(i,1,n)
+         if(p[i][j-1]!=-1)p[i][j]=p[p[i][j-1]][j-1];
+}
+int lca(int u,int v)
+{
+    int ans;
+    if(l[u]<l[v])swap(u,v);
+    for(int j=19;j>=0;j--)
+        if(p[u][j]!=-1&&l[p[u][j]]>=l[v])u=p[u][j];
+    if(u==v)return u;
+    for(int j=19;j>=0;j--)
+        if(p[u][j]!=p[v][j])u=p[u][j],v=p[v][j];
+    return p[u][0];
+}
+
+void get(int u,int P)
+{
+    for(auto v:a[u])
+    {
+        if(v==P)continue;
+        else {
+            get(v,u);
+            ans[m[{min(u,v),max(u,v)}]]=ck[v];
+            ck[u]+=ck[v];
+        }
+    }
+}
+ int main()
+{
+
+//cout<<Pow((ll)1000000,(ll)110);
+//freopen("262144.in","r",stdin);
+//freopen("262144.out","w",stdout);
+//    char input[15];
+//    scanf("%s", &input);  array of char
+//  gets(c+1);  array of char
+//printf("%s, ",name[k[i]].c_str());  printf of string
+//priority_queue<ll,vector<ll>,greater<ll>>y;
+input;
+
+cin>>n;
+int u,v;
+forr(i,1,n-1)
+{
+    cin>>u>>v;
+    if(u>v)swap(u,v);
+    m[{u,v}]=i;
+    a[u].push_back(v);
+    a[v].push_back(u);
+}
+memset(p,-1,sizeof p);
+dfs(1,-1);
+pre();
+int q;
+cin>>q;
+while(q--){
+    cin>>u>>v;
+    int LCA=lca(u,v);
+    ck[u]++;
+    ck[v]++;
+    ck[LCA]-=2;
+}
+get(1,-1);
+forr(i,1,n-1)cout<<ans[i]<<" ";
+return 0;
+}
