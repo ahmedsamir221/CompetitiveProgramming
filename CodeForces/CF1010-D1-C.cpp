66ec39e48695b6cb29b0b9f3e0db25c83fe9a029
ahmedsamir221 <45498762+ahmedsamir221@users.noreply.github.com>
2019-08-02 07:44:41 +0200

    Create CF1010-D1-C.cpp

---
+++ CodeForces/CF1010-D1-C.cpp
@@ -0,0 +1,68 @@
+//https://codeforces.com/blog/entry/60851
+#pragma GCC optimize("O3")
+#include<queue>
+#include <cmath>
+#include <complex>
+#include<map>
+#include <string>
+#include<iostream>
+#include<bits/stdc++.h>
+
+
+#define pi 3.141592654
+#define forr(i,a,b) for(int i=a;i<=b;i++)
+#define F first
+#define S second
+#define input ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+typedef pair<double,double>pdd;
+typedef long long  ll;
+typedef pair<ll, ll>pii;
+typedef complex<double> point;
+//template<typename T>T gcd(T x, T y) { if(y == 0)return x; else return gcd(y, x%y); }
+//typedef bitset<30> mask;
+//int x[8]={1,0,0,-1,-1,-1,1,1};
+//int y[8]={0,1,-1,0,-1,1,-1,1};
+//#define var(x) ((x)<<1)
+//#define nvar(x) ((x)^1)
+
+/*typedef complex< double> point;
+
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+*/
+const int N=50000,M=1000000;
+const double EPS= 1e-9;
+
+
+int n,k,a[100005];
+set<int>s;
+ int main()
+{
+//freopen("travel.in","r",stdin);
+//freopen("travel.out","w",stdout);
+//__builtin_popcount()
+input;
+cin>>n>>k;
+int g=0;
+forr(i,1,n)cin>>a[i],g=__gcd(g,a[i]);
+
+for(int i=0;i<k;i++)s.insert(((ll)i*(ll)g)%k);
+cout<<s.size()<<endl;
+for(auto i:s)cout<<i<<" ";
+
+return 0;
+}
