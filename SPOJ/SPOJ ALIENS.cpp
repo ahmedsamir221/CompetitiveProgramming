dfff976e9e86df8fb8e815ac47ece75dad7e2077
ahmedsamir221 <45498762+ahmedsamir221@users.noreply.github.com>
2019-08-20 02:18:57 +0200

    Create SPOJ ALIENS.cpp

---
+++ SPOJ/SPOJ ALIENS.cpp
@@ -0,0 +1,130 @@
+//https://youtu.be/Fa69kqT9NPY?list=PLPt2dINI2MIY7l5zyFd1W28rei3b-AXaJ&t=1105
+#pragma GCC optimize("O3")
+#include<queue>
+#include <cmath>
+#include <complex>
+#include<map>
+#include <string>
+#include<iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include<bits/stdc++.h>
+
+
+#define pi 3.141592654
+#define forr(i,a,b) for(int i=a;i<=b;i++)
+#define F first
+#define S second
+#define input ios_base::sync_with_stdio(0);cin.tie(0);
+
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+using namespace std;
+typedef pair<double,double>pdd;
+typedef long long  ll;
+typedef pair<ll, ll>pii;
+typedef complex<double> point;
+
+//int x[8]={1,0,0,-1,-1,-1,1,1};
+//int y[8]={0,1,-1,0,-1,1,-1,1};
+const int N=1000,M=1000000;
+const double EPS= 1e-9;
+const int MAX = 100000+9;
+int n;
+point pnts[MAX], r[3], cen;
+double rad;
+int ps, rs;	// ps = n, rs = 0, initially
+
+
+bool intersectSegments(point a, point b, point c, point d, point & intersect) {
+  double d1 = cp(a - b, d - c), d2 = cp(a - c, d - c), d3 = cp(a - b, a - c);
+  if (fabs(d1) < EPS)
+    return false;  // Parllel || identical
+
+  double t1 = d2 / d1, t2 = d3 / d1;
+  intersect = a + (b - a) * t1;
+
+  if (t1 < -EPS || t1 > 1+ EPS || t2 < -EPS || t2 > 1 + EPS)
+    return false;  //e.g ab is ray, cd is segment ... change to whatever
+  return true;
+}
+
+pair<double, point> findCircle(point a, point b, point c) {
+	//create median, vector, its prependicular
+	point m1 = (b+a)*0.5, v1 = b-a, pv1 = point(v1.Y, -v1.X);
+	point m2 = (b+c)*0.5, v2 = b-c, pv2 = point(v2.Y, -v2.X);
+	point end1 = m1+pv1, end2 = m2+pv2, center;
+	intersectSegments(m1, end1, m2, end2, center);
+	return make_pair( length(a-center), center );
+}
+
+
+void MEC() {
+	if(ps == 0 && rs == 2) {
+		cen = (r[0]+r[1])/2.0;
+		rad = length(r[0]-cen);
+	}
+	else if(rs == 3) {
+		pair<double, point> p = findCircle(r[0], r[1], r[2]);
+		cen = p.second;
+		rad = p.first;
+	}
+	else if(ps == 0) {
+		cen = r[0];	// sometime be garbage, but will not affect
+		rad = 0;
+	}
+	else {
+		ps--;
+		MEC();
+
+		if(length(pnts[ps]-cen) > rad) {
+			r[rs++] = pnts[ps];
+			MEC();
+			rs--;
+		}
+
+		ps++;
+	}
+}
+ int main()
+{
+//freopen("travel.in","r",stdin);
+//freopen("travel.out","w",stdout);
+//__builtin_popcount()
+input;
+int t;
+cin>>t;
+while(t--)
+{
+    cin>>n;
+    double x,y;
+    for(int i=0;i<n;i++)
+    {
+       cin>>x>>y;
+       pnts[i]={x,y};
+    }
+    ps=n,rs=0;
+    MEC();
+    cout<<fixed<<setprecision(2);
+    cout<<rad<<endl;
+    cout<<cen.X<<" "<<cen.Y<<endl;
+}
+return 0;
+}
